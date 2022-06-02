47b43816db5926fe8b556ffb926f978362893260
rbolt13 <rbolt@pdx.edu>
2022-06-02 12:59:37 -0700

    add scrape total stats file

---
+++ 00_R/00_scrape_total_stats.R
@@ -0,0 +1,7 @@
+#' Scrape Total Stats Data
+#' 
+#' @description this file uses the R package rvest to 
+#' scrape total stats data from Basketball Reference. 
+#' Link: https://www.basketball-reference.com/ 
+#' 
+#'
